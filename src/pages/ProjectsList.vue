8452629c0a6bfb5c01338d17f8cf3375f04bf9b4
gianlucafalaschi <gian100gian@outlook.it>
2024-08-06 17:57:46 +0200

    add: pagination

--- src/pages/ProjectsList.vue
+++ src/pages/ProjectsList.vue
@@ -6,25 +6,40 @@ export default {
   name: 'ProjectsList',
   data() {
     return {
-      projects: []
+      projects: [],
+      currentPage: 1,
+      nextPageUrl: null,
+      prevPageUrl: null
     }
   },
   components: {
     ProjectCard
   },
   methods: {
-    getProjectsFromApi() {
+    getProjectsFromApi(dataPage) {
       // Funzione che prende i projects dall'API 
-      axios.get('http://127.0.0.1:8000/api/projects')
+      axios.get('http://127.0.0.1:8000/api/projects', {
+        params: {
+          page: dataPage
+        }
+      })
         .then((response) => {
           //console.log(response);
-          this.projects = response.data.results;
+          this.projects = response.data.results.data;
+          //aggiorno variabile currentPage perndendo i risultati della chiamata Api
+          this.currentPage = response.data.results.current_page;
+
+          this.nextPageUrl = response.data.results.next_page_url;
+          
+          this.prevPageUrl = response.data.results.prev_page_url;
+
+
         });
 
     }
   },
   mounted() {
-    this.getProjectsFromApi()
+    this.getProjectsFromApi(this.currentPage);
   }
 }
 </script>
@@ -41,9 +56,14 @@ export default {
     <!-- Pagination -->
     <nav aria-label="Page navigation example">
       <ul class="pagination">
-        <li class="page-item"><a class="page-link">Previous</a></li>
-
-        <li class="page-item"><a class="page-link">Next</a></li>
+        <!--se c'è prevPageUrl, stampo il pulsante per la pagina precedente -->
+        <li v-if="prevPageUrl" class="page-item">
+          <a class="page-link" @click="getProjectsFromApi(currentPage - 1)">Previous</a>
+        </li>
+        <!--se c'è nextPageUrl, stampo il pulsante per la pagina successiva -->
+        <li v-if="nextPageUrl" class="page-item">
+          <a class="page-link" @click="getProjectsFromApi(currentPage + 1)">Next</a>
+        </li>
       </ul>
     </nav>  
     <!-- / Pagination -->
