f11fd46e375073b3fe40054f6633333e93ca9233
gianlucafalaschi <gian100gian@outlook.it>
2024-10-04 16:27:03 +0200

    fix: AppHome

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -10,12 +10,42 @@ export default {
 </script>
 
 <template>
+  <section class="ms-hero d-flex justify-content-center align-items-center shadow">
+    <div class="ms-inside-hero text-white text-center">
+      <h1 class="mb-5">Designing the Future, One Website at a Time</h1>
+      <router-link class="btn btn-dark shadow" :to="{name: 'projects'}">Go to our Projects </router-link>
+    </div>
+  </section>
   <div class="container ms-min-height">
-    <h1>Welcome to our site</h1>
+    <h1 class="mb-3">Welcome to YourProjects</h1>
+    <p>At <strong>YourProjects</strong>, we specialize in creating innovative, user-friendly websites that help businesses thrive in the digital age. Our team of expert designers and developers work closely with you to understand your vision and bring it to life with cutting-edge technology and creative design.</p>
+
+    <p>We offer a comprehensive range of services, including:</p> 
+
+    <p><strong>Custom Website Design</strong>: Tailored to reflect your brand's unique identity and goals.</p>
+    <p><strong>Responsive Development</strong>: Ensuring your site looks and functions perfectly on all devices.</p>
+    <p><strong>E-commerce Solutions</strong>: Building robust online stores that drive sales and enhance customer experience.</p>
+    <p><strong>SEO Optimization</strong>: Enhancing your website's visibility and ranking on search engines.</p>
+    <p><strong>Maintenance & Support</strong>: Providing ongoing support to keep your website running smoothly and securely.</p>
+    <p>Our mission is to empower businesses by delivering exceptional web solutions that not only meet but exceed expectations.</p>
+    <p class="fs-5"><strong>Join us in designing the future, one website at a time.</strong></p>
   </div>
   
 </template>
 
-<style>
+<style scoped lang="scss">
+.ms-hero {
+  width: 100%;
+  height: 400px;
+  background-image: url(../assets/pexels-8moments-1323550.jpg);
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
 
+.ms-inside-hero {
+  background-color: rgba(0, 0, 0, 0.157);
+  border-radius: 10px;
+  padding: 20px;
+}
 </style>
